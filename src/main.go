33b8ebe5c011af10e1361f0c3d3141be78fff532
chez-shanpu <cheztomo513@gmail.com>
2019-11-17 11:49:34 +0900

    Add output process

--- src/main.go
+++ src/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"github.com/chez-shanpu/repo2tree/src/model"
+	"log"
+	"os"
+)
 
-func main(){
-	fmt.Println("Hello World")
+func main() {
+	var repoRootPath string
+	var repositoryName string
+	var language string
+	var getDate string
+	var outputName string
+	var rootNode *model.Node
+
+	treeRoot := []string{repoRootPath}
+	rootNode = MakeLayer(treeRoot, 1, nil)
+	nodeInfo := model.NodeInfo{
+		RootNode:       rootNode,
+		RepositoryName: repositoryName,
+		Language:       language,
+		GetDate:        getDate,
+	}
+	outputJson(outputName, nodeInfo)
+	log.Printf("Output to %v completed", outputName)
+}
+
+func outputJson(outputName string, nodeInfo model.NodeInfo) {
+	file, err := os.Create(outputName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	bytes, _ := json.Marshal(nodeInfo)
+	file.Write(bytes)
 }
